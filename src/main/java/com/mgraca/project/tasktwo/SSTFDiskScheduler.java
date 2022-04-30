3f77f37c1c19eb99058d9a97609cd572d8ec3ac6
matthewgraca <mgraca@cpp.edu>
2022-04-30 13:24:54 -0700

    added boilerplate for disk scheduler

---
+++ src/main/java/com/mgraca/project/tasktwo/SSTFDiskScheduler.java
@@ -0,0 +1,13 @@
+package com.mgraca.project.tasktwo;
+
+public class SSTFDiskScheduler{
+  /**
+   * calcuates the total head movement and amount of pivots
+   * @param head  the initial position of the disk head
+   * @param queue the given cylinder requests
+   * @return  an array where the first value is the total head movement and the second value is the amount of pivots
+   */
+  public static int[] calculate(int head, int[] queue){
+    return new int[]{236, 2};
+  }
+}
--- src/test/java/com/mgraca/project/DiskSchedulerTest.java
+++ src/test/java/com/mgraca/project/DiskSchedulerTest.java
@@ -1,9 +1,52 @@
-package com.mgraca.project;
+package com.mgraca.project.tasktwo;
 
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import java.util.Arrays;
 
 public class DiskSchedulerTest{
+  @Test
+  public void testCSCANExample1(){
+    int head = 53;
+    int[] queue = {98,183,37,122,14,124,65,67};
+    int[] expected = {382, 2};
+    int[] actual = CSCANDiskScheduler.calculate(head, queue); 
+    String errorMsg = "Expected: " + Arrays.toString(expected) + ", returned: " + Arrays.toString(actual);
 
+    assertTrue(errorMsg, Arrays.equals(expected, actual));
+  }
+
+  @Test
+  public void testFCFSExample1(){
+    int head = 53;
+    int[] queue = {98,183,37,122,14,124,65,67};
+    int[] expected = {640, 6};
+    int[] actual = FCFSDiskScheduler.calculate(head, queue); 
+    String errorMsg = "Expected: " + Arrays.toString(expected) + ", returned: " + Arrays.toString(actual);
+
+    assertTrue(errorMsg, Arrays.equals(expected, actual));
+  }
+
+  @Test
+  public void testSCANExample1(){
+    int head = 53;
+    int[] queue = {98,183,37,122,14,124,65,67};
+    int[] expected = {236, 1};
+    int[] actual = SCANDiskScheduler.calculate(head, queue); 
+    String errorMsg = "Expected: " + Arrays.toString(expected) + ", returned: " + Arrays.toString(actual);
+
+    assertTrue(errorMsg, Arrays.equals(expected, actual));
+  }
+
+  @Test
+  public void testSSTFExample1(){
+    int head = 53;
+    int[] queue = {98,183,37,122,14,124,65,67};
+    int[] expected = {236, 2};
+    int[] actual = SSTFDiskScheduler.calculate(head, queue); 
+    String errorMsg = "Expected: " + Arrays.toString(expected) + ", returned: " + Arrays.toString(actual);
+
+    assertTrue(errorMsg, Arrays.equals(expected, actual));
+  }
 }
