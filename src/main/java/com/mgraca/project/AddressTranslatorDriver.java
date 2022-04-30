c747830a5a92c5b530160a95ef9294ce09115d78
matthewgraca <mgraca@cpp.edu>
2022-04-30 02:18:08 -0700

    added drivers for task 1 and 3, and made a folder that will be turned in for the assignment

---
+++ src/main/java/com/mgraca/project/AddressTranslatorDriver.java
@@ -0,0 +1,14 @@
+package com.mgraca.project;
+
+import java.util.Scanner;
+
+public class AddressTranslatorDriver{
+  public static void main (String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter page size: ");
+    int pageSize = input.nextInt();
+    System.out.print("Enter a virtual address: ");
+    int address = input.nextInt();
+    System.out.println("The address " + address + " contains: " + "page number = " + AddressTranslator.computePageNumber(pageSize, address) + " offset = " + AddressTranslator.computeOffset(pageSize, address)); 
+  }
+}
