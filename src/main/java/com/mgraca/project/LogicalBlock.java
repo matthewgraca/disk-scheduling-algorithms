85f35fbe2cbcb001cb28915f9182c9a7ad81c085
matthewgraca <mgraca@cpp.edu>
2022-04-30 01:42:22 -0700

    added logical block addressing code and tests

--- src/main/java/com/mgraca/project/LogicalBlock.java
+++ src/main/java/com/mgraca/project/LogicalBlock.java
@@ -1,5 +1,20 @@
 package com.mgraca.project;
 
 public class LogicalBlock{
-
+  /**
+   *  calculates the hard disk location of a logical block address
+   *  @param  lba the logical block address
+   *  @param  cylinders the number of cylinders in the disk
+   *  @param  tracks  the number of tracks in the disk
+   *  @param  sectors the number of sectors in the disk
+   *  @return the location of the logical block address in the hard disk
+   */
+  public static int[] calculateHDLocation(int lba, int cylinders, int tracks, int sectors){
+    /* note that real-world implementations start at <0,0,1>, but ours starts at <0,0,0>,
+    thus we don't need to add 1 to sectorNum*/
+    int cylinderNum = lba / (tracks * sectors);
+    int trackNum = lba / sectors % tracks;
+    int sectorNum = lba % sectors;
+    return new int[] {cylinderNum, trackNum, sectorNum};
+  }
 }
--- src/test/java/com/mgraca/project/LogicalBlockTest.java
+++ src/test/java/com/mgraca/project/LogicalBlockTest.java
@@ -4,6 +4,14 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class LogicalBlockTest{
+import java.util.Arrays;
 
+public class LogicalBlockTest{
+  @Test
+  public void example1(){
+    int[] expected = {0, 16, 33};
+    int[] actual = LogicalBlock.calculateHDLocation(1041, 1000, 20, 63); 
+    String errorMsg = "Expected: " + Arrays.toString(expected) + ", returned: " + Arrays.toString(actual);
+    assertTrue(errorMsg, Arrays.equals(expected, actual));
+  }
 }
