c262570bc4b8ed7c186693f5c5eb6066ab5acca0
matthewgraca <mgraca@cpp.edu>
2022-04-30 16:28:09 -0700

    implemented FCFS

--- src/main/java/com/mgraca/project/tasktwo/FCFSDiskScheduler.java
+++ src/main/java/com/mgraca/project/tasktwo/FCFSDiskScheduler.java
@@ -8,6 +8,28 @@ public class FCFSDiskScheduler{
    * @return  an array where the first value is the total head movement and the second value is the amount of pivots
    */
   public static int[] calculate(int head, int[] queue){
-    return new int[]{640, 6};
+    // empty queue
+    if (queue.length == 0){
+      return new int[]{0, 0};
+    }
+    // queue with 1 item
+    else if (queue.length == 1){
+      return new int[]{Math.abs(head - queue[0]), 0};
+    }
+    // queue with > 1 item
+    else{
+      int sum = Math.abs(queue[0] - head);
+      boolean pathRight = head - queue[0] > 0; // if path is going right, 1. if path goes left, 0
+      int pivots = 0;
+      for (int i = 0; i < queue.length - 1; i++){
+        // if path is right and the trajectory is left, or the path is left and the trajectory is right then pivot
+        if (pathRight && queue[i] - queue[i+1] < 0 || !pathRight && queue[i] - queue[i+1] > 0){
+          pivots++;
+          pathRight = !pathRight;
+        }
+        sum += Math.abs(queue[i] - queue[i+1]);
+      }
+      return new int[]{sum, pivots};
+    }
   }
 }
